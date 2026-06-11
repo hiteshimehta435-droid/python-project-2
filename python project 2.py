af8c2478c713387823f17e43bb6f319e656cc899
hiteshimehta435-droid <hiteshimehta435@gmail.com>
2026-06-11 17:05:03 +0530

    Add files via upload

---
+++ python project 2.py
@@ -0,0 +1,57 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+================== RESTART: C:\Users\bscom\.ipython\py.2.p.py ==================
+Welcome to the Generate Right-Angled Triangle pattern and Analyze Number!
+... 
+1. Generate Right-Angled Triangle Pattern
+2. Analyze Numbers
+3. Exit
+Enter your choice: 1
+Enter number of rows: 7
+
+Right-Angled Triangle Pattern:
+*
+**
+***
+****
+*****
+******
+*******
+Welcome to the Generate Right-Angled Triangle pattern and Analyze Number!
+1. Generate Right-Angled Triangle Pattern
+2. Analyze Numbers
+3. Exit
+Enter your choice: 2
+Enter starting number: 1
+Enter ending number: 20
+
+Number Analysis:
+1 - Other
+2 - Even
+3 - Divisible by 3
+4 - Even
+5 - Other
+6 - Even
+7 - Other
+8 - Even
+9 - Divisible by 3
+10 - Even
+11 - Other
+12 - Even
+13 - Other
+14 - Even
+15 - Divisible by 3
+16 - Even
+17 - Other
+18 - Even
+19 - Other
+20 - Even
+Welcome to the Generate Right-Angled Triangle pattern and Analyze Number!
+1. Generate Right-Angled Triangle Pattern
+2. Analyze Numbers
+3. Exit
+Enter your choice: 3
+Program Exited Successfully.
+Thank you
+Have good day
